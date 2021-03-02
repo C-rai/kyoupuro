5f47ed81fb6a82c74809bfe7c94d388a2d1ca452
C-rai <73065279+C-rai@users.noreply.github.com>
2021-03-02 22:39:58 +0900

    Create abc122d.c

---
+++ abc122d.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+enum{
+  A,
+  C,
+  G,
+  T
+};
+
+int main(void){
+  int n;
+  scanf("%d",&n);
+
+  int dp[110][5] = {0};
+  
+  for(int i = 0;i < 4;i ++){
+    dp[0][i] = 1;
+  }
+
+  for(int i = 0;i < 110;i ++){
+    for(int j = 0;j < 4;j ++){
+      //A
+      dp[i+1][A] += dp[i][j];
+      //C
+      dp[i+1][C] += dp[i][j];
+      //AGC
+      if(j == G){
+        if(i-1 >= 0){
+          dp[i+1][C] -= dp[i-1][A];
+        }
+      }
+      //G
+      dp[i+1][G] += dp[i][j];
+      //T
+      dp[i+1][T] += dp[i][j];
+    }
+  }
+
+  printf("%d\n",dp[n-1][A]+dp[n-1][G]+dp[n-1][C]+dp[n-1][T]);
+
+  return 0;
+  
+}
