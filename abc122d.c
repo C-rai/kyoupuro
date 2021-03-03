e7252c2708c853693377fb624795f284d6459246
C-rai <73065279+C-rai@users.noreply.github.com>
2021-03-03 22:17:31 +0900

    Update abc122d.c

--- abc122d.c
+++ abc122d.c
@@ -1,55 +1,44 @@
-#include<stdio.h>
+#include <stdio.h>
 
-enum{
-  A,
-  C,
-  G,
-  T
-};
+#define Mod 1000000007
 
-int main(void){
-  int n;
-  scanf("%d",&n);
-
-  int dp[110][5] = {0};
-  
-  for(int i = 0;i < 4;i ++){
-    dp[0][i] = 1;
-  }
-
-  for(int i = 0;i < 100;i ++){
-    for(int j = 0;j < 4;j ++){
-      //A
-      dp[i+1][A] += dp[i][j];
-      //C
-      dp[i+1][C] += dp[i][j];
-      //AGC
-      if(j == G){
-        if(i-1 >= 0){
-          dp[i+1][C] -= dp[i-1][A];
-        }
-      }
-      //GAC
-      else if(j == A){
-        if(i-1 >= 0){
-          dp[i+1][C] -= dp[i-1][G];
-        }
-      }
-      //G
-      dp[i+1][G] += dp[i][j];
-      //ACG
-      if(j == C){
-        if(i -1 >= 0){
-          dp[i+1][G] -= dp[i-1][A];
-        }
-      }
-      //T
-      dp[i+1][T] += dp[i][j];
-    }
-  }
-
-  printf("%d\n",dp[n-1][A]+dp[n-1][G]+dp[n-1][C]+dp[n-1][T]);
-
-  return 0;
-  
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	
+	int i, j, k, l;
+	long long dp[101][4][4][4] = {};
+	for (j = 0; j < 4; j++) {
+		for (k = 0; k < 4; k++) {
+			for (l = 0; l < 4; l++) dp[3][j][k][l] = 1;
+		}
+	}
+	dp[3][0][1][2] = 0;
+	dp[3][0][2][1] = 0;
+	dp[3][2][0][1] = 0;
+	for (i = 4; i <= N; i++) {
+		for (j = 0; j < 4; j++) {
+			for (k = 0; k < 4; k++) {
+				for (l = 0; l < 4; l++) {
+					if (j == 0 && k == 1 && l == 2) continue;
+					else if (j == 0 && k == 2 && l == 1) continue;
+					else if (j == 2 && k == 0 && l == 1) continue;
+					
+					if ((k == 2 && l == 1) || (j == 2 && l == 1)) dp[i][j][k][l] = (dp[i-1][1][j][k] + dp[i-1][2][j][k] + dp[i-1][3][j][k]) % Mod;
+					else dp[i][j][k][l] = (dp[i-1][0][j][k] + dp[i-1][1][j][k] + dp[i-1][2][j][k] + dp[i-1][3][j][k]) % Mod;
+				}
+			}
+		}
+	}
+	
+	long long ans = 0;
+	for (j = 0; j < 4; j++) {
+		for (k = 0; k < 4; k++) {
+			for (l = 0; l < 4; l++) ans += dp[N][j][k][l];
+		}
+	}
+	printf("%lld\n", ans % Mod);
+	fflush(stdout);
+	return 0;
 }
